41d24c0aa08492d3d6456f8af92533de1568ce87
Aaron Carrizal <aaronk4572@gmail.com>
2024-09-19 21:01:48 -0600

    Added ability yo open several streams in separate windows

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
-import path from 'path';
+import { app, BrowserWindow, ipcMain, screen } from 'electron'
 
-let mainWindow;
+let mainWindow
 
 app.on('ready', () => {
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const { width, height } = primaryDisplay.workAreaSize
+
+  console.log(`Screen width: ${width}`)
+  console.log(`Screen height: ${height}`)
+
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: width / 4,
+    height: height / 2,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
-  });
+  })
 
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile('index.html')
 
-  // Maneja la creación de nuevas ventanas para los streams
   ipcMain.on('open-stream-window', (event, streamURL) => {
     const streamWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      frame: false, // Hacer la ventana sin marco
+      width: width / 2,
+      height: width / 4,
+      frame: false,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false
       }
-    });
+    })
 
-    // Cargar una página que contenga el stream
-    streamWindow.loadURL(streamURL);
-  });
-});
+    streamWindow.loadFile('stream.html')
+
+    streamWindow.webContents.on('did-finish-load', () => {
+      streamWindow.webContents.send('load-stream', streamURL)
+    })
+  })
+})
